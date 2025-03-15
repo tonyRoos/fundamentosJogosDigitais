4efe9237169b8f82663e2b27c8fca92932f12c19
roosInteractive <roosinteractive@gmail.com>
2025-03-15 10:20:12 -0300

    Aula 2

---
+++ Aulas/Code/CameraBehaviour.gd
@@ -0,0 +1,12 @@
+extends Camera2D
+
+@export var player: CharacterBody2D
+@export var min_position: Vector2
+@export var max_position:Vector2
+
+var camera_position: Vector2
+func _process(delta: float) -> void:
+	camera_position.x = clamp( player.transform.origin.x, min_position.x, max_position.x)
+	camera_position.y = clamp( player.transform.origin.y, min_position.y, max_position.y)
+	
+	transform.origin = camera_position
